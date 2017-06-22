1d492d4cd6d5ebb29ceb573813f334bb7d3b6f56
Davide Tampellini <tampe125@gmail.com>
2017-06-22 09:57:29 +0200

    Ported function to enforce minimum execution time

--- angie/installation/framework/timer/timer.php
+++ angie/installation/framework/timer/timer.php
@@ -94,6 +94,116 @@ class ATimer
 	}
 
 	/**
+	 * Enforce the minimum execution time
+	 *
+	 * @param    bool $log             Should I log what I'm doing? Default is true.
+	 * @param    bool $serverSideSleep Should I sleep on the server side? If false we return the amount of time to wait in msec
+	 *
+	 * @return  int Wait time to reach min_execution_time in msec
+	 */
+	public function enforce_min_exec_time($log = true, $serverSideSleep = true)
+	{
+		// Try to get a sane value for PHP's maximum_execution_time INI parameter
+		if (@function_exists('ini_get'))
+		{
+			$php_max_exec = @ini_get("maximum_execution_time");
+		}
+		else
+		{
+			$php_max_exec = 10;
+		}
+		if (($php_max_exec == "") || ($php_max_exec == 0))
+		{
+			$php_max_exec = 10;
+		}
+		// Decrease $php_max_exec time by 500 msec we need (approx.) to tear down
+		// the application, as well as another 500msec added for rounding
+		// error purposes. Also make sure this is never gonna be less than 0.
+		$php_max_exec = max($php_max_exec * 1000 - 1000, 0);
+		
+		$minexectime = $this->min_exec_time;
+		if (!is_numeric($minexectime))
+		{
+			$minexectime = 0;
+		}
+
+		// Make sure we are not over PHP's time limit!
+		if ($minexectime > $php_max_exec)
+		{
+			$minexectime = $php_max_exec;
+		}
+
+		// Get current running time
+		$elapsed_time = $this->getRunningTime() * 1000;
+
+		$clientSideSleep = 0;
+
+		// Only run a sleep delay if we haven't reached the minexectime execution time
+		if (($minexectime > $elapsed_time) && ($elapsed_time > 0))
+		{
+			$sleep_msec = $minexectime - $elapsed_time;
+
+			if (!$serverSideSleep)
+			{
+				ALog::_(ANGIE_LOG_DEBUG, "Asking client to sleep for $sleep_msec msec");
+				$clientSideSleep = $sleep_msec;
+			}
+			elseif (function_exists('usleep'))
+			{
+				if ($log)
+				{
+					ALog::_(ANGIE_LOG_DEBUG, "Sleeping for $sleep_msec msec, using usleep()");
+				}
+
+				usleep(1000 * $sleep_msec);
+			}
+			elseif (function_exists('time_nanosleep'))
+			{
+				if ($log)
+				{
+					ALog::_(ANGIE_LOG_DEBUG, "Sleeping for $sleep_msec msec, using time_nanosleep()");
+				}
+				$sleep_sec  = floor($sleep_msec / 1000);
+				$sleep_nsec = 1000000 * ($sleep_msec - ($sleep_sec * 1000));
+
+				time_nanosleep($sleep_sec, $sleep_nsec);
+			}
+			elseif (function_exists('time_sleep_until'))
+			{
+				if ($log)
+				{
+					ALog::_(ANGIE_LOG_DEBUG, "Sleeping for $sleep_msec msec, using time_sleep_until()");
+				}
+
+				$until_timestamp = time() + $sleep_msec / 1000;
+
+				time_sleep_until($until_timestamp);
+			}
+			elseif (function_exists('sleep'))
+			{
+				$sleep_sec = ceil($sleep_msec / 1000);
+
+				if ($log)
+				{
+					ALog::_(ANGIE_LOG_DEBUG, "Sleeping for $sleep_sec seconds, using sleep()");
+				}
+				
+				sleep($sleep_sec);
+			}
+		}
+		elseif ($elapsed_time > 0)
+		{
+			// No sleep required, even if user configured us to be able to do so.
+			if ($log)
+			{
+				ALog::_(ANGIE_LOG_DEBUG, "No need to sleep; execution time: $elapsed_time msec; min. exec. time: $minexectime msec");
+			}
+		}
+
+		return $clientSideSleep;
+	}
+
+	/**
 	 * Reset the timer
 	 */
 	public function resetTime()
