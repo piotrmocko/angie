4c07269eefa5df98d8d6d3335962f15555844fde
Davide Tampellini <tampe125@gmail.com>
2017-07-06 09:18:05 +0200

    Fixed resume replacement

--- angie/platforms/wordpress/views/replacedata/tmpl/default.php
+++ angie/platforms/wordpress/views/replacedata/tmpl/default.php
@@ -139,7 +139,7 @@ echo $this->loadAnyTemplate('steps/steps');
                     <span class="icon-cancel"></span>
 					<?php echo AText::_('SESSION_BTN_CANCEL'); ?>
                 </button>
-                <button class="btn btn-success btn-small" onclick="replacements.resumeBackup(); return false;">
+                <button class="btn btn-success btn-small" onclick="replacements.resumeReplacement(); return false;">
                     <span class="icon-redo"></span>
 					<?php echo AText::_('SETUP_REPLACE_TEXT_BTNRESUME'); ?>
                 </button>
