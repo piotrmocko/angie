0414e4ca47f2f4e6da49454f5bef12dd4ec492ce
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-10-15 10:16:17 +0200

    Refactoring the installer build

---
+++ build/templates/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package   AkeebaBackup
+ * @copyright Copyright (c)2006-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+define('AKEEBA_PRO', '##PRO##');
+define('AKEEBA_VERSION', '##VERSION##');
+define('AKEEBA_DATE', '##DATE##');
